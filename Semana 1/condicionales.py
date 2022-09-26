868beed468f417714a3ddcd3ec6564e21cc2f244
avend547 <joseavendano95@gmail.com>
2022-09-26 14:22:34 -0400

    epa

---
+++ Semana 1/condicionales.py
@@ -0,0 +1,6 @@
+number1 = float(input("Number to be divided: "))
+number2 = float(input("Divider: "))
+if number2 == 0:
+    print("Divider must be different from 0")
+else:
+    print(number1/number2)
