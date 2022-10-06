ddf26c056ddb7bcd5383dda6b7622012431032ce
avend547 <joseavendano95@gmail.com>
2022-10-06 13:09:43 -0400

    ahora si somos

--- Semana 2/Raizdigital.py
+++ Semana 2/Raizdigital.py
@@ -2,7 +2,17 @@ pos_integer = input("write a positive integer: ")
 dig = [int(x) for x in pos_integer]
 dig_root=sum(dig)
 if dig_root > 9:
+    while dig_root > 9:
+        cycle = sum([int(i) for i in str(dig_root)])
+        dig_root = cycle
+        print(cycle)
+else:
+    print(dig_root)
+
+"""
+if dig_root > 9:
     cycle = sum([int(i) for i in str(dig_root)])
     print(cycle)
 else:
     print(dig_root)
+"""
